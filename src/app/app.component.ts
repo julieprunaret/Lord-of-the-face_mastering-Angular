90e97739ae9bbde8e1241b066a13b1bd620e181e
julieprunaret <j.prunaret@gmail.com>
2022-04-18 10:15:12 +0200

    properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  mySnap1!: FaceSnap;
+  mySnap2!: FaceSnap;
+  mySnap3!: FaceSnap;
+
+  ngOnInit(){
+    this.mySnap1 = new FaceSnap(
+      "Gandalf",
+      "Un magicien aussi sympathique que puissant",
+      "assets/gandalf.png",
+      new Date(),
+      0
+    ),
+    this.mySnap2 = new FaceSnap(
+      "Sam",
+      "Le véritable héro du Seigneur des Anneaux",
+      "assets/sam.png",
+      new Date(),
+      0
+    ),
+    this.mySnap3 = new FaceSnap(
+      "Gollum",
+      "un personnage \"2 en 1\"",
+      "assets/gollum.png",
+      new Date(),
+      0
+    )
+
+  }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -8,30 +8,19 @@ import { FaceSnap } from '../models/face-snap.model';
 })
 export class FaceSnapComponent implements OnInit{
   @Input() faceSnap!: FaceSnap;
-
-  title!: string;
-  description!: string;
-  creationDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
   buttonText!:string;
 
   ngOnInit(): void {
-      this.title = "Gandalf";
-      this.description = "Un magicien aussi sympathique que puissant";
-      this.creationDate = new Date();
-      this.snaps = 6;
-      this.imageUrl = "assets/gandalf.png"
       this.buttonText= "Like";
   }
 
   onSnap(){
       if(this.buttonText === "Like") {
-        this.snaps++;
+        this.faceSnap.snaps++;
         this.buttonText= "Dislike";
       } else {
         this.buttonText= "Like";
-        this.snaps--;
+        this.faceSnap.snaps--;
     }
   }
 
