3c01a997be2c58e8dec33818e8654fc4e09db383
julieprunaret <j.prunaret@gmail.com>
2022-04-18 16:01:47 +0200

    dynamic style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,22 +14,22 @@ export class AppComponent implements OnInit{
     {
       title: "Gandalf",
       description: "Un magicien aussi sympathique que puissant",
-      imageUrl: "assets/gandalf.png",
+      imageUrl: "assets/gandalf.jpg",
       creationDate: new Date(),
-      snaps: 0,
+      snaps: 12,
       location: "Fondcombe",
     },
     {
       title: "Sam",
       description: "Le véritable héro du Seigneur des Anneaux",
-      imageUrl: "assets/sam.png",
+      imageUrl: "assets/sam.jpg",
       creationDate: new Date(),
-      snaps: 0
+      snaps: 130
     },
     {
       title: "Gollum",
       description: "un personnage \"2 en 1\"",
-      imageUrl: "assets/gollum.png",
+      imageUrl: "assets/gollum.webp",
       creationDate: new Date(),
       snaps: 0,
       location: "Mordor",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
