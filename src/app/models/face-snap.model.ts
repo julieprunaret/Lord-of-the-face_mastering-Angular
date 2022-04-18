6ab371615b6e83db53ef1c3cd0e3f8cc83c57796
julieprunaret <j.prunaret@gmail.com>
2022-04-18 11:08:26 +0200

    conditions

--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,10 +1,8 @@
 export class FaceSnap {
-    constructor(
-        public title: string, 
-        public description: string, 
-        public imageUrl: string,
-        public creationDate: Date,
-        public snaps: number,
-        ){
-    }
+    title!: string; 
+    description!: string; 
+    imageUrl!: string;
+    creationDate!: Date;
+    snaps!: number;
+    location?: string;
 }
