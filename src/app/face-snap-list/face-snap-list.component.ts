0b79114766ea9a7fda72a0df43ddb215f1cd4f05
julieprunaret <j.prunaret@gmail.com>
2022-04-18 18:15:56 +0200

    service creation

---
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snap.service';
+
+
+@Component({
+  selector: 'app-face-snap-list',
+  templateUrl: './face-snap-list.component.html',
+  styleUrls: ['./face-snap-list.component.scss']
+})
+export class FaceSnapListComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
+  constructor(
+    private FaceSnapsService: FaceSnapsService
+  ) { }
+
+  ngOnInit(): void {
+    this.faceSnaps = this.FaceSnapsService.faceSnaps;
+  }
+
+}
